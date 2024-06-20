abc640cf97f42c0afd361e6293cd191de44b5b55
franpelirrojo <franfc.00@gmail.com>
2024-06-20 18:10:29 +0200

    Treesitter configurado

--- lua/franpelirrojo/lazy/init.lua
+++ lua/franpelirrojo/lazy/init.lua
@@ -1,9 +1,31 @@
 return{
     { 
-        "ellisonleao/gruvbox.nvim", 
+        'ellisonleao/gruvbox.nvim', 
         config = function()
             vim.o.background = "light"
             vim.cmd([[colorscheme gruvbox]])
+        end
+    },
+    {
+        --[[
+        --  Treesitter, al menos en mi ordenar, requiere clang y las herramientas
+        --  de LLVM. Está configurado pra que mantenga actualizado c, cpp y lua,
+        --  y proporcione las funcionalidades dásicas de tabulación y coloreado.
+        --  La etiqueta de gcc est´apor sia caso clang no se puede ejecutar, que lo
+        --  intente con gcc.
+        --]]
+        'nvim-treesitter/nvim-treesitter',
+        config = function ()
+            local install = require('nvim-treesitter.install')
+            install.prefer_git = false
+            install.compilers = { "clang", "gcc" }
+
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "c", "cpp", "lua" },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
         end
     }
 }
