ef25b951e29c09ad3e63173c5fbd7c2d91cd2d56
franpelirrojo <franfc.00@gmail.com>
2024-06-20 18:32:41 +0200

    Configurar harpoon

--- lua/franpelirrojo/lazy/init.lua
+++ lua/franpelirrojo/lazy/init.lua
@@ -44,6 +44,19 @@ return{
         -- Dependencia de Telescope para un mejor rendimiento.
         'nvim-telescope/telescope-fzf-native.nvim',
         build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' 
+    },
+    {
+        'ThePrimeagen/harpoon',
+        config = function()
+            local mark = require("harpoon.mark")
+            local ui = require("harpoon.ui")
+
+            vim.keymap.set("n", "<leader>a", mark.add_file)
+            vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+            vim.keymap.set("n", "<C-t>", function() ui.nav_file(1) end)
+            vim.keymap.set("n", "<C-h>", function() ui.nav_file(2) end)
+        end
     }
 }
 
