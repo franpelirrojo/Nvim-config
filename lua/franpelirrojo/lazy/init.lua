0885b8c8dc3610d78180a773e97904f217f04733
franpelirrojo <franfc.00@gmail.com>
2024-06-20 18:26:53 +0200

    Configurar telescope

--- lua/franpelirrojo/lazy/init.lua
+++ lua/franpelirrojo/lazy/init.lua
@@ -27,5 +27,23 @@ return{
                 indent = { enable = true },
             })
         end
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- Project files
+            vim.keymap.set('n', '<leader>ps', function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") }); -- Project search
+            end)
+        end
+    },
+    { 
+        -- Dependencia de Telescope para un mejor rendimiento.
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' 
     }
 }
+
